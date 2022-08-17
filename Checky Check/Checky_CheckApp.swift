7848fdb905b5ccd5fceaf2f05d2d115803de5ee7
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2022-08-18 00:24:17 +0900

    Initial Commit

---
+++ Checky Check/Checky_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checky_CheckApp.swift
+//  Checky Check
+//
+//  Created by Oliver Park on 2022/08/18.
+//
+
+import SwiftUI
+
+@main
+struct Checky_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
